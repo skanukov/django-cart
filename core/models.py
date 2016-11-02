1852493a84e94f061615b63325bfcb5dd7aff9bf
Sergey Kanukov <s.kanukov@gmail.com>
2016-11-02 20:31:54 +0300

    Add 'core' to apps and create basic 'Brand' model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
