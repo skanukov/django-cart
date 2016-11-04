046694519c6d9ec69374286352811bc987cf9853
Sergey Kanukov <s.kanukov@gmail.com>
2016-11-04 14:41:49 +0300

    Translate other model fields

--- core/models.py
+++ core/models.py
@@ -20,17 +20,20 @@ class Taxonomy(models.Model):
 
 class Taxon(models.Model):
     name = models.CharField(max_length=200, verbose_name=_('name'))
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True,
+                                              verbose_name=_('description'))
     image = models.CharField(max_length=200, null=True, blank=True)
-    permalink = models.URLField(max_length=200, null=True, blank=True)
+    permalink = models.URLField(max_length=200, null=True, blank=True,
+                                verbose_name=_('permalink'))
     position = models.IntegerField(default=0, verbose_name=_('position'))
     meta_title = models.CharField(max_length=500, null=True, blank=True)
     meta_keywords = models.CharField(max_length=500, null=True, blank=True)
     meta_description = models.CharField(max_length=500, null=True, blank=True)
 
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
-                                       blank=True)
-    taxonomy = models.ForeignKey(Taxonomy, on_delete=models.CASCADE) 
+                                       blank=True, verbose_name=_('parent'))
+    taxonomy = models.ForeignKey(Taxonomy, on_delete=models.CASCADE,
+                                           verbose_name=_('taxonomy')) 
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
